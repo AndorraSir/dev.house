c4fe354654b0af7b02122c66b5b4ae07caf49096
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-05-02 12:20:46 +0200

    More commits for setting

---
+++ application/logs/log-2018-05-02.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-02 12:19:14 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 73
+ERROR - 2018-05-02 12:19:16 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 73
