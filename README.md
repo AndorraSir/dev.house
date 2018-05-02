e2f4f4ad94cadc8390510c3763bb8e97b548e349
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-05-02 15:39:25 +0200

    More application fixes

---
+++ README.md
@@ -0,0 +1,1 @@
+# house.dev.ad
--- templates/bootstrap2-responsive/form_fields/SMART_SEARCH.php
+++
@@ -1,1 +0,0 @@
-<input id="search_option_smart" type="text" class="span6 form-control"   value="{search_query}" placeholder="{lang_CityorCounty}" autocomplete="off" />
