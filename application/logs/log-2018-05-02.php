681428ae04fdad3cac3ad7bacf17c272a1e34ea6
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-05-02 13:22:33 +0200

    database config

--- application/logs/log-2018-05-02.php
+++ application/logs/log-2018-05-02.php
@@ -2,3 +2,8 @@
 
 ERROR - 2018-05-02 12:19:14 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 73
 ERROR - 2018-05-02 12:19:16 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 73
+ERROR - 2018-05-02 12:37:51 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 73
+ERROR - 2018-05-02 13:20:31 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 73
+ERROR - 2018-05-02 13:21:31 --> Severity: Warning  --> mysql_connect(): Operation timed out /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 73
+ERROR - 2018-05-02 13:21:31 --> Unable to connect to the database
+ERROR - 2018-05-02 13:21:31 --> Severity: 8192  --> mysql_connect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 73
