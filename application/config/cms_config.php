1f84edee6ffd45f1ae030a7e7fae7293d696f817
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-05-18 13:02:32 +0200

    Logout button

--- application/config/cms_config.php
+++ application/config/cms_config.php
@@ -13,10 +13,6 @@ $config['version'] = '1.6.2';
 
 $config['default_template_css'] = 'assets/css/style.css';
 
-$config['codecanyon_username'] = 'fmonkey';
-
-$config['codecanyon_code'] = '3b0c84d6-9854-4fb0-a078-24d45208cc80';
-
 $config['hash_function'] = 'hash';
 
 $config['installed'] = true;
--- application/views/admin/components/page_head_main.php
+++ application/views/admin/components/page_head_main.php
@@ -87,6 +87,12 @@
     <script src="<?php echo base_url('admin-assets/js/jquery.mjs.nestedSortable.js');?>"></script>
     <script src="<?php echo base_url('admin-assets/js/jquery.helpers.js');?>"></script>
     
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     
   <script type="text/javascript">
     // Calendar translation start //
@@ -126,6 +132,8 @@
     <?php if($stats_enabled): ?>
     <script src="http://ljiljan.com.hr/stats_real_estate.php?url=<?php echo base_url(); ?>&f=gallery.js"></script>
     <?php endif; ?>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
     <link rel="stylesheet" href="<?php echo base_url('admin-assets/style/blueimp-gallery.min.css')?>">
     <!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
