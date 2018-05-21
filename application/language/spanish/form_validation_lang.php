fd95863fcda528ea97fb890cb4d41164d50c3557
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-05-21 12:48:31 +0200

    More language updates

--- application/language/spanish/form_validation_lang.php
+++ application/language/spanish/form_validation_lang.php
@@ -1,29 +1,28 @@
-<?php
+<?php 
 
-$lang['required']			= "Se requirere % de campo.";
-$lang['isset']				= "El % de campo debe contener un valor.";
-$lang['valid_email']		= "El % de campo debe contener una dirección válida de correo eléctrónico.";
-$lang['valid_emails']		= "El % de campo debe contener todas las direcciones válidas de correo eléctrónico.";
-$lang['valid_url']			= "El % de campo debe contener una URL válida.";
-$lang['valid_ip']			= "El % de campo debe contener una IP válida.";
-$lang['min_length']			= "El % de campo debe ser al menos como el % de caracteres en longitud.";
-$lang['max_length']			= "El % de campo no puede exceder al % de caracteres de longitud.";
-$lang['exact_length']		= "El % de campo debe ser exactamente el % de caracteres de longitud.";
-$lang['alpha']				= "El % de campo debe contener sólo caracteres alfabéticos.";
-$lang['alpha_numeric']		= "El % de campo debe contener sólo caracteres numéricos.";
-$lang['alpha_dash']			= "El % de campo solo puede contener caracteres numéricos, guiones.";
-$lang['numeric']			= "El % de campo debe contener sólo números.";
-$lang['is_numeric']			= "El % de campo debe contener sólo caracteres numéricos.";
-$lang['integer']			= "El % de campo debe contener un número entero.";
-$lang['regex_match']		= "El % de campo no tiene el formato correcto.";
-$lang['matches']			= "El % de campo no coincide con el % de campo.";
-$lang['is_unique'] 			= "El % de campo debe contener un único valor.";
-$lang['is_natural']			= "El % de campo debe contener sólo números positivos.";
-$lang['is_natural_no_zero']	= "El % de campo debe ser un número mayor que cero.";
-$lang['decimal']			= "El % de campo debe contener un número decimal.";
-$lang['less_than']			= "El % de campo debe contener un número inferioir que el %.";
-$lang['greater_than']		= "El % de campo debe contener un número mayor que el %.";
+$lang['required'] = "Se requirere %s de campo.";
+$lang['isset'] = "El %s de campo debe contener un valor.";
+$lang['valid_email'] = "El %s de campo debe contener una dirección válida de correo eléctrónico.";
+$lang['valid_emails'] = "El %s de campo debe contener todas las direcciones válidas de correo eléctrónico.";
+$lang['valid_url'] = "El %s de campo debe contener una URL válida.";
+$lang['valid_ip'] = "El %s de campo debe contener una IP válida.";
+$lang['min_length'] = "El %s de campo debe ser al menos como el %s de caracteres en longitud.";
+$lang['max_length'] = "El %s de campo no puede exceder al %s de caracteres de longitud.";
+$lang['exact_length'] = "El %s de campo debe ser exactamente el %s de caracteres de longitud.";
+$lang['alpha'] = "El %s de campo debe contener sólo caracteres alfabéticos.";
+$lang['alpha_numeric'] = "El %s de campo debe contener sólo caracteres numéricos.";
+$lang['alpha_dash'] = "El %s de campo solo puede contener caracteres numéricos, guiones.";
+$lang['numeric'] = "El %s de campo debe contener sólo números.";
+$lang['is_numeric'] = "El %s de campo debe contener sólo caracteres numéricos.";
+$lang['integer'] = "El %s de campo debe contener un número entero.";
+$lang['regex_match'] = "El %s de campo no tiene el formato correcto.";
+$lang['matches'] = "El %s de campo no coincide con el %s de campo.";
+$lang['is_unique'] = "El %s de campo debe contener un único valor.";
+$lang['is_natural'] = "El %s de campo debe contener sólo números positivos.";
+$lang['is_natural_no_zero'] = "El %s de campo debe ser un número mayor que cero.";
+$lang['decimal'] = "El %s de campo debe contener un número decimal.";
+$lang['less_than'] = "El %s de campo debe contener un número inferioir que el %s.";
+$lang['greater_than'] = "El %s de campo debe contener un número mayor que el %s.";
+$lang['exists'] = "La %s no se encontró en la base de datos";
 
-
-/* End of file form_validation_lang.php */
-/* Location: ./system/language/english/form_validation_lang.php */
+?>
---
+++ application/logs/log-2018-05-21.php
@@ -0,0 +1,10 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2018-05-21 12:47:41 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2018-05-21 12:47:41 --> Severity: Notice  --> mysql_pconnect(): send of 5 bytes failed with errno=32 Broken pipe /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2018-05-21 12:47:41 --> Severity: Notice  --> Undefined index: AGENT_COUNTY_AFFILIATE /Applications/MAMP/htdocs/dev.house/application/views/admin/user/index.php 91
+ERROR - 2018-05-21 12:47:41 --> Severity: Notice  --> Undefined index: AGENT_COUNTY_AFFILIATE /Applications/MAMP/htdocs/dev.house/application/views/admin/user/index.php 92
+ERROR - 2018-05-21 12:47:44 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2018-05-21 12:47:44 --> Severity: Notice  --> Undefined index: AGENT_COUNTY_AFFILIATE /Applications/MAMP/htdocs/dev.house/application/views/admin/user/index.php 91
+ERROR - 2018-05-21 12:47:44 --> Severity: Notice  --> Undefined index: AGENT_COUNTY_AFFILIATE /Applications/MAMP/htdocs/dev.house/application/views/admin/user/index.php 92
+ERROR - 2018-05-21 12:47:49 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 91
