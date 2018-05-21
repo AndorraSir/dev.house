d6ee354fbb0330f095629bf81eab21d8e58fdff6
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-05-21 13:01:17 +0200

    More language updates

--- application/logs/log-2018-05-21.php
+++ application/logs/log-2018-05-21.php
@@ -8,3 +8,4 @@ ERROR - 2018-05-21 12:47:44 --> Severity: 8192  --> mysql_pconnect(): The mysql 
 ERROR - 2018-05-21 12:47:44 --> Severity: Notice  --> Undefined index: AGENT_COUNTY_AFFILIATE /Applications/MAMP/htdocs/dev.house/application/views/admin/user/index.php 91
 ERROR - 2018-05-21 12:47:44 --> Severity: Notice  --> Undefined index: AGENT_COUNTY_AFFILIATE /Applications/MAMP/htdocs/dev.house/application/views/admin/user/index.php 92
 ERROR - 2018-05-21 12:47:49 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 91
+ERROR - 2018-05-21 12:51:48 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead /Applications/MAMP/htdocs/dev.house/system/database/drivers/mysql/mysql_driver.php 91
--- system/language/spanish/form_validation_lang.php
+++ system/language/spanish/form_validation_lang.php
@@ -1,29 +1,27 @@
-<?php
+<?php 
 
-$lang['required']			= "The %s field is required.";
-$lang['isset']				= "The %s field must have a value.";
-$lang['valid_email']		= "The %s field must contain a valid email address.";
-$lang['valid_emails']		= "The %s field must contain all valid email addresses.";
-$lang['valid_url']			= "The %s field must contain a valid URL.";
-$lang['valid_ip']			= "The %s field must contain a valid IP.";
-$lang['min_length']			= "The %s field must be at least %s characters in length.";
-$lang['max_length']			= "The %s field can not exceed %s characters in length.";
-$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
-$lang['alpha']				= "The %s field may only contain alphabetical characters.";
-$lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
-$lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
-$lang['numeric']			= "The %s field must contain only numbers.";
-$lang['is_numeric']			= "The %s field must contain only numeric characters.";
-$lang['integer']			= "The %s field must contain an integer.";
-$lang['regex_match']		= "The %s field is not in the correct format.";
-$lang['matches']			= "The %s field does not match the %s field.";
-$lang['is_unique'] 			= "The %s field must contain a unique value.";
-$lang['is_natural']			= "The %s field must contain only positive numbers.";
-$lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
-$lang['decimal']			= "The %s field must contain a decimal number.";
-$lang['less_than']			= "The %s field must contain a number less than %s.";
-$lang['greater_than']		= "The %s field must contain a number greater than %s.";
+$lang['required'] = "El campo %s es requerido";
+$lang['isset'] = "El campo %s debe tener un valor";
+$lang['valid_email'] = "El campo %s debe contener una direccion de email valida";
+$lang['valid_emails'] = "El campo %s debe contener direcciones de email validas";
+$lang['valid_url'] = "el campo %s debe contener una URL valida";
+$lang['valid_ip'] = "El campo %s debe contener una IP valida";
+$lang['min_length'] = "The %s field must be at least %s characters in length.";
+$lang['max_length'] = "El campo %s no puede exceder %s caracteres de largo";
+$lang['exact_length'] = "El campo %s debe contener %s caracteres exactos";
+$lang['alpha'] = "El campo %s debe contener solo caracteres alfabeticos";
+$lang['alpha_numeric'] = "El campo %s debe contener caracteres alfanumericos";
+$lang['alpha_dash'] = "El campo %s debe contener caracteres alfanumericos, guiones bajos y guines";
+$lang['numeric'] = "El campo %s debe contener solo numeros";
+$lang['is_numeric'] = "El campo %s debe contener solo caracteres numericos";
+$lang['integer'] = "El campo %s debe contener un entero";
+$lang['regex_match'] = "El campo %s no esta en el formato correcto";
+$lang['matches'] = "El campo %s no concuerda con el campo %s";
+$lang['is_unique'] = "El campo %s debe contener un valor unico";
+$lang['is_natural'] = "El campo %s debe contener solo numeros positivos";
+$lang['is_natural_no_zero'] = "El campo %s debe contener un numero mayor a 0";
+$lang['decimal'] = "El campo %s debe contener un numero decimal";
+$lang['less_than'] = "El campo %s debe contener un numero menor a %s";
+$lang['greater_than'] = "El campo %s debe contener un numero mayor a %s";
 
-
-/* End of file form_validation_lang.php */
-/* Location: ./system/language/english/form_validation_lang.php */
+?>
