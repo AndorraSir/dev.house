283305cc88f658001b829e1d161148855081e5f5
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-10-24 14:35:02 +0200

    Map fixes

---
+++ application/logs/log-2018-10-24.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2018-10-24 13:58:59 --> Severity: Notice  --> Undefined variable: id /Applications/MAMP/htdocs/dev.house/application/controllers/admin/settings.php 60
+ERROR - 2018-10-24 13:59:27 --> Severity: Notice  --> Undefined variable: id /Applications/MAMP/htdocs/dev.house/application/controllers/admin/settings.php 60
+ERROR - 2018-10-24 13:59:27 --> Severity: Notice  --> Undefined variable: id /Applications/MAMP/htdocs/dev.house/application/controllers/admin/settings.php 60
+ERROR - 2018-10-24 14:09:05 --> Severity: Notice  --> Undefined variable: id /Applications/MAMP/htdocs/dev.house/application/controllers/admin/settings.php 60
+ERROR - 2018-10-24 14:09:05 --> Severity: Notice  --> Undefined variable: id /Applications/MAMP/htdocs/dev.house/application/controllers/admin/settings.php 60
+ERROR - 2018-10-24 14:10:53 --> Severity: Notice  --> Trying to get property of non-object /Applications/MAMP/htdocs/dev.house/application/views/admin/forms/edit.php 73
+ERROR - 2018-10-24 14:10:53 --> Severity: Notice  --> Trying to get property of non-object /Applications/MAMP/htdocs/dev.house/application/views/admin/forms/edit.php 88
+ERROR - 2018-10-24 14:15:31 --> Severity: Notice  --> Undefined variable: id /Applications/MAMP/htdocs/dev.house/application/controllers/admin/settings.php 60
+ERROR - 2018-10-24 14:15:42 --> Severity: Notice  --> Undefined variable: id /Applications/MAMP/htdocs/dev.house/application/controllers/admin/settings.php 60
