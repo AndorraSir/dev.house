157292397c176a2a7de86bd70099223d5695caa3
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-11-14 13:41:48 +0100

    changes to admin and item homepage

--- templates/bootstrap2-responsive/widgets/results_item.php
+++ templates/bootstrap2-responsive/widgets/results_item.php
@@ -32,17 +32,17 @@ if(isset($columns) && $columns == 3)
         <div class="prop-details">
 
          <?php if(!empty($item['option_36'])):?>
-        <span class="price" style="display:block; height:25px;"><?php _che($options_prefix_36); ?> <?php echo _ch($item['option_36']); ?><?php _che($options_suffix_36, ''); ?></span>
+        <span class="price mb-3"><?php _che($options_prefix_36); ?> <?php echo _ch($item['option_36']); ?><?php _che($options_suffix_36, ''); ?></span>
         <?php endif;?>
          <?php if(!empty($item['option_37'])):?>
           <span class="price"><?php _che($options_prefix_37); ?> <?php echo _ch($item['option_37']); ?><?php _che($options_suffix_37, ''); ?>&nbsp;</span>
           <?php endif;?>
         </div>
-        <div class=""><span class="f_<?php echo _ch($item['is_featured']); ?>"><?php echo _ch($item['address']); ?></span></div>
-        <div class="prop-description "><i><?php echo _ch($item['option_chlimit_8']); ?></i></div>
+        <div class="prop-address hide"><span>{lang_Address}</span><?php echo _ch($item['address']); ?></span></div>
+        <div class="prop-description mb-3"><?php echo _ch($item['option_chlimit_8']); ?></div>
 
         <?php if(!empty($item['icons'])):?>
-          <p class="prop-icons" style="display: block">
+          <p class="prop-icons" style="display: none">
               <?php 
                   foreach ($item['icons'] as $icon) {
                       echo $icon['icon'];
@@ -54,14 +54,13 @@ if(isset($columns) && $columns == 3)
           </p>
           <?php endif;?>
 
-        <div class="row">
 
-          <div class="col-sm-4"><?php echo _ch($options_name_2); ?><?php echo _ch($item['icons']); ?> <br><span><?php echo _ch($item['option_2']); ?></span></div>
-          <div class="col-sm-4"><?php echo _ch($options_name_3); ?> <br><span><?php echo _ch($item['option_3']); ?></span></div>
-          <div class="col-sm-4"><?php echo _ch($options_name_19); ?> <br><span><?php echo _ch($item['option_19']); ?></span></div>
-          
+        <div class="row details mb-3">
+            <div class="col-sm-4"><span><?php echo _ch($options_name_20); ?></span><span><?php echo _ch($item['option_20']); ?></span></div>
+            <div class="col-sm-4"><span><?php echo _ch($options_name_19); ?> </span><span><?php echo _ch($item['option_19']); ?></span></div>
 
-      </div>
+            <div class="col-sm-4"><span>m2</span><span><?php echo _ch($item['option_57']); ?></span></div>      
+        </div>
         
         <div class="prop-details">
           <a class="btn btn-info col-sm-12" href="<?php echo _ch($item['url']); ?>">
