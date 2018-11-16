5b3c9ce5264496e63acf47070a91ce8c8dedf537
Mona Fouad <monafouad@MacBook-Pro-2.local>
2018-11-16 11:26:06 +0100

    attempt 4

---
+++ application/logs/log-2018-10-29.php
@@ -0,0 +1,3 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2018-10-29 00:08:28 --> Severity: Notice  --> Undefined variable: title /Applications/MAMP/htdocs/dev.house/templates/bootstrap2-responsive/widgets/center_recentproperties_list.php 2
---
+++ templates/bootstrap2-responsive/widgets/footer_logo.php
@@ -0,0 +1,5 @@
+<div class="col-sm-4 pl-0">
+    <div class="logo-transparent">
+        <img src="<?php echo $website_logo_url_inverted; ?>" alt="Logo footer" />
+    </div>
+</div>
